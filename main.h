90e5995d3b915a02dd8ae7104d490322363c1dcd
KingHawkins <kinghawkins61@gmail.com>
2022-10-18 00:57:24 -0700

    Adding task 0

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char * format);
+int _printf(const char * format, ...);
+int _putchar(char c);
+void decimal_to_baseN(char *converted, unsigned int num, int base);
 
 #endif
