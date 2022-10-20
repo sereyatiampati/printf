3182f0e8d1b64b57d85a3156d24dda4aba780056
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-20 12:17:53 -0700

    more task done

---
+++ _printf_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _printf_char - Function print values type char.
+ * @arguments: In arguments list.
+ * Return: 1.
+*/
+int _printf_char(va_list arguments)
+{
+	char char_to_print = (char)va_arg(arguments, int);
+
+	_putchar (char_to_print);
+	return (1);
+}
